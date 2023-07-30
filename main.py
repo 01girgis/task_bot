c39c3a607ece3238d5d5382c00ed2fa8886a1fe8
crus <crus@cruss-iMac.local>
2023-07-30 05:32:00 +0300

    code updated with revised solution

--- main.py
+++ main.py
@@ -1,41 +1,62 @@
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-from langchain.agents import create_csv_agent
-from langchain.llms import  OpenAI
+import openai
+import pandas as pd
+from pandasai.llm.openai import OpenAI
+from pandasai import PandasAI
+import os
 from dotenv import load_dotenv
-import  os 
 
-###load enviroment variables
-load_dotenv()
+load_dotenv() 
 
-def manin():
- #check the key 
+def main():
+ # check the key 
  if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
         print("OPENAI_API_KEY is not set")
         exit(1)
  else:
         print("OPENAI_API_KEY is set")
 
+# reading csv files
+ data1 = pd.read_csv('data/US_INDU_CALCULATIONS.csv')
+ data2 = pd.read_csv('data/US_INDU_INCOME_STATEMENT.csv')
 
-###Gather Docs for langchain agent 
- docs = ["data/US_INDU_BALANCE_SHEET_STATEMENT.csv"]
-###Set CSV Agent and Function Input Func.
- agent = create_csv_agent(OpenAI(temperature=0),docs,verbose = True)
+#  merging the required files by outer order
+ df = pd.merge(data1, data2,  
+                   on=['name','fiscal_year'], 
+                   how='outer')
 
+# set dataframe for financial factuals
+ df = df[['name','fiscal_year','netincome','totalrevenue','pricetoearnings','dividendyield','roe']]
+
+# filter data  by 2022
+ df_f = df[(df['fiscal_year'] == 2022)]
+ #print(df_f.head(30)) #test
+
+ 
+ 
  def answer_question(user_question: str) -> str: 
-       agent.run(user_question)
+  llm = OpenAI(api_token=openai.api_key)
+  pandas_ai = PandasAI(llm)
+  print(pandas_ai(df_f, prompt= user_question+" with  financial advise"))
+  #(Test)print(pandas_ai(df_f, prompt='What is Total Revenue of Home Depot Inc 2022?'))
+  #(Test)prompt = f" What is return on equity of walmart inc  2022? in {df_f} and financial calculation advise"
+  #(Test)
+#   prompt = f" {user_question} in {df_f} and financial calculation advise"
+#   response = openai.Completion.create(
+#      engine="text-davinci-003",
+#      prompt=prompt,
+#      temperature=0,
+#      max_tokens=100
+#     )
+#   print(response.choices[0].text.strip())
 
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-# char_spliter  = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap = 0)
-# chunk = char_spliter.create_documents(docs)
-# ##print(len(chunk))
-
-### data process loop
- while input != "quit()":
+# data process loop
+ while True:
   user_input = input("Ask a question, Your AdvisorBot : ")
-  response = answer_question(user_input)
-  print(response)
-
+  #bot terminate method
+  if user_input == "exit":
+        break
+  answer_question(user_input)
 
 
 if __name__ == "__main__" :
-     manin()
+     main()
